0812d004c350ca9cd9c9411baf68eeaf9914faa7
natalietsvia-sys <natalietsvia@gmail.com>
2026-05-09 23:11:53 +0300

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,57 @@
+package week8;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String company;
+    private String title;
+
+    @Embedded
+    private Name name;
+
+    protected Contact() {
+    }
+
+    public Contact(String company, String title, Name name) {
+        this.company = company;
+        this.title = title;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}

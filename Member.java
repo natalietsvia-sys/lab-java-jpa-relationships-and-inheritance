ae7b738e35ecba526eff7e1a3888b79bfda1b83c
natalietsvia-sys <natalietsvia@gmail.com>
2026-05-09 23:16:20 +0300

    Create Member.java

---
+++ Member.java
@@ -0,0 +1,47 @@
+package week8;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    private LocalDate renewalDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "division_id")
+    private Division division;
+
+    protected Member() {
+    }
+
+    public Member(String name, MemberStatus status, LocalDate renewalDate) {
+        this.name = name;
+        this.status = status;
+        this.renewalDate = renewalDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setDivision(Division division) {
+        this.division = division;
+    }
+}

a0680bc9edd14ea291f29664fb890d6a78f24f25
natalietsvia-sys <natalietsvia@gmail.com>
2026-05-09 23:23:25 +0300

    Create Guest.java

---
+++ Guest.java
@@ -0,0 +1,28 @@
+package week8;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Guest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private GuestStatus status;
+
+    protected Guest() {
+    }
+
+    public Guest(String name, GuestStatus status) {
+        this.name = name;
+        this.status = status;
+    }
+}

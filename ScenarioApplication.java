98afc9ae741704dfeb58bbddc070704701dda532
natalietsvia-sys <natalietsvia@gmail.com>
2026-05-09 23:10:15 +0300

    Create ScenarioApplication.java

---
+++ ScenarioApplication.java
@@ -0,0 +1,11 @@
+package week8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScenarioApplication.class, args);
+    }
+}
